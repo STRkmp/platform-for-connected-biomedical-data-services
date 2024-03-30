afd8af3c2829852c8d90a31ef6adc2d1bda1010b
Anarchy <derectwib@gmail.com>
2024-03-30 16:06:34 +0500

    работает наконец nginx. общая статика с nginx. избавился от consumer-web, добавил отдельным приложением в django

--- backend/backend_project/api/publish_utils/publisher.py
+++
@@ -1,8 +0,0 @@
-from .config import qmanager
-import json
-
-
-def publishMessage(data: bytes, queue_name: str = None, headers: dict = None):
-    message = json.dumps(data)
-    queue_name = data.__class__.__name__
-    qmanager.publish(data= message, queue_name= queue_name, headers= headers)
--- backend_project/api/publish_utils/publisher.py
+++ backend_project/api/publish_utils/publisher.py
@@ -1,8 +1,8 @@
 from .config import qmanager
-import json
+import jsonpickle
 
 
 def publishMessage(data: bytes, queue_name: str = None, headers: dict = None):
-    message = json.dumps(data)
+    message = jsonpickle.encode(data, unpicklable=False)
     queue_name = data.__class__.__name__
     qmanager.publish(data= message, queue_name= queue_name, headers= headers)
